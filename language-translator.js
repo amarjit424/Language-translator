c2bf2f9eccee81cf07b43e3e093bdcd72d083b76
amarjit424 <amarjityadav1a2@gmail.com>
2025-02-25 21:20:14 +0530

    Create language-translator.js

---
+++ language-translator.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const englishdata = document.getElementById('englishdata');
+    const translatebtn = document.getElementById('translatebtn');
+
+    englishdata.addEventListener('input', function() {
+        let words = englishdata.value.split(' ');
+        for (let i = 0; i < words.length; i++) {
+            words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        }
+        englishdata.value = words.join(' ');
+    });
+
+    let translate = async() => {
+        try{
+            let language = document.getElementById('language');
+            let url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${language.value}&dt=t&q=${englishdata.value}`;
+            let res = await fetch(url);
+            let data = await res.json();
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
+            }
+            let translateddata = data[0][0][0];
+            let hindidata = document.getElementById('hindidata');
+            hindidata.value = translateddata;
+        } catch(err) {
+            console.log('Sorry Disable To Get The Data');
+        }
+    }
+
+    translatebtn.addEventListener('click', translate);
+
+    document.getElementById('copy').addEventListener('click', () => {
+        let hindidata = document.getElementById('hindidata');
+        hindidata.select();
+        document.execCommand('copy');
+        alert('Copied');
+        document.contentType = 'text/plain';
+    });
+});
